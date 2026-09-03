c695f45816a7e927f54169c40a384260ec036fd4
Fábio neri <fabioneri201550@gmail.com>
2026-09-03 17:45:39 -0300

    Update Supabase URL and app version in config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.VIMAK_CONFIG = {
   backend: "supabase",
-  supabaseUrl: "https://kpsoigxkpwfcdgdvwmngd.supabase.co",
+  supabaseUrl: "https://kpsoigxkpwfcddgvwmgd.supabase.co",
   supabasePublishableKey: "sb_publishable_gbTa-v_gAn37JhzRek1B_A_8_a1BxYn",
-  appVersion: "6.0.0"
+  appVersion: "6.2.0"
 };
